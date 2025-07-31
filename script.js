c205df6a078a33ebc16c3a6ca8bf3d6d421be309
MohammedAnsari123 <ansarimohammed213161@gmail.com>
2025-07-31 18:51:32 +0530

    updated

--- script.js
+++ script.js
@@ -1,4 +1,56 @@
 console.log("Lets Write Java Script")
+let currentSong = new Audio();
 
+async function getSongs() {
+    let a = await fetch("http://127.0.0.1:3000/songs/")
+    // let a = await fetch("songs")
+    let response = await a.text()
+    let div = document.createElement("div")
+    div.innerHTML = response;
+    let as = div.getElementsByTagName("a")
+    let songs = []
+    for (let index = 0; index < as.length; index++) {
+        const element = as[index];
+        if (element.href.endsWith(".mp3")) {
+            songs.push(element.href.split("/songs/")[1])
+        }
+    }
+    return songs
+}
 
+const playMusic = (track) => {
+    // let audio = new Audio("/songs/" + track)
+    currentSong.src = "/songs/" + track
+    currentSong.play()
+}
+
+async function main() {
+    // Get the list of all the songs
+    let songs = await getSongs()
+
+    // Show all the songs in the Playlist
+    let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0]
+    for (const song of songs) {
+        songUL.innerHTML = songUL.innerHTML + `<li>
+            <img src="icones/MusicIconWhite.svg" alt="MusicIconWhite">
+            <div class="info">
+                <div>${song.replaceAll("%20", " ")}</div>
+                <div>Song Artist</div>
+            </div>
+            <div class="playnow">
+                <span>Play Now</span>
+                <img src="icones/PlayButtonWhite.svg" alt="">
+            </div>
+        </li>`;
+    }
+    // Attach an event listener to each song
+    Array.from(document.querySelector(".songList").getElementsByTagName("li")).forEach(e => {
+        e.addEventListener("click", element => {
+            console.log(e.querySelector(".info").firstElementChild.innerHTML)
+            playMusic(e.querySelector(".info").firstElementChild.innerHTML.trim())
+        })
+    })
+}
+
+main()
 
